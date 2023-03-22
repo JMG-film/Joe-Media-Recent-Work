dce7d95a9c4384f627229851bafddbae44b7d22a
JoeMediaGroup <104857713+JMG-film@users.noreply.github.com>
2023-03-22 14:37:42 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// const cursorRounded = document.querySelector('.Cursor');
+
+
+
+// const moveCursor = (e)=> {
+//   const mouseY = e.clientY;
+//   const mouseX = e.clientX;
+   
+//   cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+// }
+
+// window.addEventListener('mousemove', moveCursor)
+
+$(document).ready(function(){
+ var cursor = $('.cursor');
+
+ $(window).mousemove(function(e) {
+        cursor.css({
+            top: e.clientY - cursor.height() / 2,
+            left: e.clientX - cursor.width() / 2
+        });
+    });
+
+$(window).mousemove(function(e) {
+        cursor.css({
+            top: e.clientY - cursor.height() / 2,
+            left: e.clientX - cursor.width() / 2
+        });
+    });
+
+    $(window)
+        .mouseleave(function() {
+            cursor.css({
+                opacity: "0"
+            });
+        })
+        .mouseenter(function() {
+            cursor.css({
+                opacity: "1"
+            });
+        });
+
+      $('.video')
+        .mouseleave(function() {
+          cursor.css({
+            transform: "scale(1)"
+          });
+        })
+        .mouseenter(function() {
+          cursor.css({
+            transform: "scale(0)"
+          });
+        })
+});
